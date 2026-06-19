34cecd1c190ea23fad2d5e7c242a2a4254a67157
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-06-19 08:21:44 +0700

    [incoming] cleanup incoming messages periodically

---
+++ app/src/main/java/me/capcom/smsgateway/ui/settings/IncomingSettingsFragment.kt
@@ -0,0 +1,28 @@
+package me.capcom.smsgateway.ui.settings
+
+import android.os.Bundle
+import android.text.InputType
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
+import me.capcom.smsgateway.R
+
+class IncomingSettingsFragment : BasePreferenceFragment() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.incoming_preferences, rootKey)
+    }
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        when (preference.key) {
+            "incoming.lifetime_days" -> {
+                (preference as EditTextPreference).setOnBindEditTextListener {
+                    it.inputType = InputType.TYPE_CLASS_NUMBER
+                    it.setSelectAllOnFocus(true)
+                    it.selectAll()
+                }
+            }
+        }
+
+        super.onDisplayPreferenceDialog(preference)
+    }
+}
